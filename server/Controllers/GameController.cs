23fcbb9453887ba7551121c185090b5e592cbfef
Scott Guertin <sguertin@gmail.com>
2020-10-27 16:31:18 -0400

    Initial commit of scaffolding

---
+++ server/Controllers/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class GameController : ControllerBase
+    {        
+        private readonly ILogger<GameController> _logger;
+
+        public GameController(ILogger<GameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "You got got";
+        }
+    }
+}
